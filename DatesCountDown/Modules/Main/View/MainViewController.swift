79c0ee3d3cc812f1d5d5fcddfd5e5b786c37e647
Artem Trubacheev <Almaz5200@gmail.com>
2019-04-15 02:17:35 +0800

    Created base for tableview

--- DatesCountDown/Modules/Main/View/MainViewController.swift
+++ DatesCountDown/Modules/Main/View/MainViewController.swift
@@ -6,20 +6,44 @@
 //  Copyright © 2019 atrubacheev. All rights reserved.
 //
 
+import RxSwift
 import UIKit
 
 class MainViewController: UIViewController {
 
     var output: MainViewOutput!
+    private var disposeBag = DisposeBag()
+
+    @IBOutlet weak var tableView: UITableView!
 
     // MARK: Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.tableFooterView = nil
     }
 }
 
 extension MainViewController: MainViewInput {
     func showCountdowns(list: [CountDown]) {
 
+    }
+}
+
+extension MainViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: R.reuseIdentifier.mainCell, for: indexPath)
+
+        return cell ?? MainCell()
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 230
     }
 }
